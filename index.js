97e7a979ae66106ec8b242afe5b5221609b66105
KavinsProjects <kavinkanagaraj16@gmail.com>
2026-06-24 11:24:39 +0530

    config_port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv, { configDotenv } from "dotenv"
 const app = express();
-const PORT = 3009;
+configDotenv()
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json()); // acceppting datafrom from frontend in jsonformat
 
